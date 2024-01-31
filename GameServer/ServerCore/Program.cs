f5cf7bff4a3bda3330211376c956e3d3d013ada8
kimhoyul <96504568+kimhoyul@users.noreply.github.com>
2024-02-01 00:39:44 +0900

    서버 추가

---
+++ GameServer/ServerCore/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ServerCore
+{
+	internal class Program
+	{
+		static void MainThread()
+		{
+			while (true)
+				Console.WriteLine("Hello Thread!");
+        }
+
+		static void Main(string[] args)
+		{
+			Thread thread = new Thread(MainThread);
+			thread.Name = "Test Thread";
+			thread.Start();
+			Console.WriteLine("Waiting for Thread");
+
+
+			thread.Join();
+			Console.WriteLine("Hello World");
+		}
+	}
+}
